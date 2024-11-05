1fe275e9dd60e0da29dc9a0dd6e65863de45a228
Rayssa Ravelia <rayssa.sibarani@gmail.com>
2024-11-05 15:46:43 +0700

    part 2: web application

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"net/http"
+	"html/template"
 )
 
 // Page structure
@@ -26,10 +28,50 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
-func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page.")}
-    p1.save()
+// Handler to display the page content
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    renderTemplate(w, "view", p)
+}
 
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))
+// Handler to edit the page content
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
+}
+
+// Handler to save the page content
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/save/"):]
+    body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    p.save()
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    err := templates.ExecuteTemplate(w, tmpl+".html", p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func main() {
+    http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
+    http.HandleFunc("/save/", saveHandler)
+
+    fmt.Println("Starting server on http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
 }
